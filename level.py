7bd16496a95ce957bc5592ce0e6afc729647717a
Felix Riddle <fedecraft3312@gmail.com>
2025-11-30 17:55:37 -0300

    Spawn player

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 
+
 class Level:
     def __init__(self):
         # Get the display surface
@@ -11,18 +12,20 @@ class Level:
         # Sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
-        
+
         # Sprite setup
         self.create_map()
-    
+
     def create_map(self):
         for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
-                
+
                 if col == "x":
-                    tile = Tile((x, y), [self.visible_sprites])
+                    tile = Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                elif col == "p":
+                    tile = Player((x, y), [self.visible_sprites])
 
     def run(self):
         # Update and draw the game
--- player.py
+++ player.py
@@ -7,3 +7,12 @@ class Player(pygame.sprite.Sprite):
 		super().__init__(groups)
 		self.image = pygame.image.load("./cache/graphics/test/player.png").convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
+
+		self.direction = pygame.math.Vector2()
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+		if keys[pygame.K_DOWN]:
+			self.direction.y = 1
